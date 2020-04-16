c281740be1f70df0aef7404e613606b3ebb49737
emilygracekz <57361805+emilygracekz@users.noreply.github.com>
2020-04-16 13:47:13 -0500

    removing custom font

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import { useFonts } from "@use-expo/font";
-import { AppLoading } from "expo";
 import WelcomeBackground from "./Components/WelcomeBackground";
 import WelcomeHeader from "./Components/WelcomeHeader";
 import WelcomeSignIn from "./Components/WelcomeSignIn";
@@ -25,23 +23,3 @@ const styles = StyleSheet.create({
   },
 });
 
-export default (props) => {
-  let [fontsLoaded] = useFonts({
-    "Quicksand-Regular": require("./assets/fonts/Quicksand-Regular.ttf"),
-    // "Quicksand-Bold": require("./assets/fonts/Quicksand-Bold.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return;
-    //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    //       <Text style={{ fontFamily: 'Inter-Black', fontSize: 40 }}>
-    //         Inter Black
-    //       </Text>
-    //       <Text style={{ fontSize: 40 }}>Platform Default</Text>
-    //     </View>
-    //   );
-    // }
-  }
-};
