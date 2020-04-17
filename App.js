08dd732bbe4a2e1f5e4bfb889aac4aa0fe1b4839
emilygracekz <57361805+emilygracekz@users.noreply.github.com>
2020-04-17 10:06:23 -0500

    adding navigation

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeBackground from "./Components/WelcomeBackground";
 import WelcomeHeader from "./Components/WelcomeHeader";
 import WelcomeSignIn from "./Components/WelcomeSignIn";
 import WelcomeSignUp from "./Components/WelcomeSignUp"
 import WelcomeLoginButton from "./Components/WelcomeLoginButton"
 
-export default App = () => {
+export default WelcomeScreen = () => {
   return (
     <View style={styles.container}>
       <WelcomeBackground />
@@ -19,6 +21,18 @@ export default App = () => {
     </View>
   );
 };
+
+const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Details" component={SelectScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
